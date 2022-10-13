88a41f050cac4b56690330b8af382fcf468b163e
l3aro <dgbao1340@gmail.com>
2022-10-13 11:39:50 +0700

    remove sec

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiKey: 'bf895d8140efcc3ec19727e86806a4d3',
+  apiKey: '',
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'https://image.tmdb.org/t/p',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiKey: 'bf895d8140efcc3ec19727e86806a4d3',
+  apiKey: '',
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'https://image.tmdb.org/t/p',
 };
